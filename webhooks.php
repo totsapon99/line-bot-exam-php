7e1f7bd72fed902ded0018dbb168c80ef16b6166
totsapon99 <48344819+totsapon99@users.noreply.github.com>
2020-05-22 14:57:55 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'ZoKTYATnWwPD2ppISqXZmsSH9L14uyTzb3ky3FAVO4sr0JQUBe0yDfa8O9ZF6hgYFCbWQ4a6xIJ5p4DnVC5cHZZwdA4JXymYdNZ2JzKC5FU+FPCLH5wSl6VD+4fiitR4V4ptpZnsbbzdeMn9E14iLQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
